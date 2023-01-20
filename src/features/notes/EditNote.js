1231dd9b4d8cfe1ec0c8551eb9e4c06c8cde2a19
ndaru <ndaru1990@gmail.com>
2023-01-20 19:54:51 +0700

    lanjut isi form note

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import {selectNoteByID} from './notesApiSLice'
+import { selectAllUsers } from '../users/usersApiSlice'
+import EditNoteForm from './EditNoteForm'
+
 
 const EditNote = () => {
-  return (
-    <div>EditNote</div>
-  )
+ const {id} = useParams()
+
+ const note = useSelector(state => selectNoteByID(state,id))
+ const users = useSelector(selectAllUsers)
+ const content = note && users ? <EditNoteForm note={note} users={users}/> : <p>Loading..</p>
+
+ return content
+  
 }
 
 export default EditNote
