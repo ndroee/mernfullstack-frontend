9db0998abbc4cffe4f2aae5fa05f23662f1052ae
ndaru <ndaru1990@outlook.com>
2023-01-20 23:49:29 +0700

    tambah form note

--- src/features/notes/EditNote.js
+++ src/features/notes/EditNote.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import {selectNoteByID} from './notesApiSLice'
-import { selectAllUsers } from '../users/usersApiSlice'
-import EditNoteForm from './EditNoteForm'
-
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectNoteById } from "./notesApiSlice";
+import { selectAllUsers } from "../users/usersApiSlice";
+import EditNoteForm from "./EditNoteForm";
 
 const EditNote = () => {
- const {id} = useParams()
+  const { id } = useParams();
 
- const note = useSelector(state => selectNoteByID(state,id))
- const users = useSelector(selectAllUsers)
- const content = note && users ? <EditNoteForm note={note} users={users}/> : <p>Loading..</p>
+  const note = useSelector((state) => selectNoteById(state, id));
+  const users = useSelector(selectAllUsers);
+  const content =
+    note && users ? (
+      <EditNoteForm note={note} users={users} />
+    ) : (
+      <p>Loading..</p>
+    );
 
- return content
-  
-}
+  return content;
+};
 
 export default EditNote
