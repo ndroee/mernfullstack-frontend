921386b206a04b349320a56a78985ba3999bdcc6
ndaru <ndaru1990@gmail.com>
2023-01-23 16:41:40 +0700

    role based page

--- src/features/auth/PersistLogin.js
+++ src/features/auth/PersistLogin.js
@@ -45,7 +45,7 @@ const PersistLogin = () => {
     console.log("error");
     content = (
       <p className="errmsg">
-        {error.data?.message}
+        {`${error?.data?.message} - `}
         <Link to="/login">Please Login Again</Link>
       </p>
     );
